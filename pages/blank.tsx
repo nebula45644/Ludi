fde5ef0cd13bf33108d5ac2cbc0398a8f1491a09
nebula45644 <127625184+nebula45644@users.noreply.github.com>
2024-03-08 10:52:56 -0600

    Update blank.tsx

--- pages/blank.tsx
+++ pages/blank.tsx
@@ -71,8 +71,6 @@ const Blank: NextPage = ({ particles }: any) => {
         <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
         <title>Ludicrous</title>
         <link rel='manifest' href='/manifest.json' />
-        <link href='/icons/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
-        <link href='/icons/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
         <link rel='apple-touch-icon' href='/icons/apple-icon.png'></link>
 
       </Head>
