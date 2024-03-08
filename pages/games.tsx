f817661eb47b51dba901ea20679ccd48b4c88652
nebula45644 <127625184+nebula45644@users.noreply.github.com>
2024-03-08 10:55:31 -0600

    Update games.tsx

--- pages/games.tsx
+++ pages/games.tsx
@@ -161,7 +161,7 @@ const Games: NextPage = ({ apps, games, bookmarks, particles, origin }: any) => 
         <meta charSet='utf-8' />
         <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
         <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover, maximum-scale=6'/>
-        <title>Games</title>
+        <title>Home View Summary</title>
         <link rel='manifest' href='/manifest.json' />
         <link rel='apple-touch-icon' href='/icons/apple-icon.png'></link>
       </Head>
