6b94257ccf5a98701cf38df05dbe04053dc1d514
nebula45644 <127625184+nebula45644@users.noreply.github.com>
2024-04-02 15:59:40 -0500

    Update _offline.tsx

--- pages/_offline.tsx
+++ pages/_offline.tsx
@@ -77,12 +77,12 @@ const Offline: NextPage = ({ particles }: any) => {
   return (
     <div className={styles.main}>
       <Head>
-        <meta name="description" content="Ludicrous | A School Site" />
+        <meta name="description" content="test" />
         <meta name="theme-color" content="#2467a5" />
         <meta charSet='utf-8' />
         <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
         <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
-        <title>Ludicrous - Offline</title>
+        <title>test- Offline</title>
         <link rel='manifest' href='/manifest.json' />
         <link href='/icons/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
         <link href='/icons/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
